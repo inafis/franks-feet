526d17e76fcdfb5d6925f67319052b52183515d7
Khalil Brown <knb6@njit.edu>
2022-01-03 17:12:44 -0500

    franks-feet-number calculations

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -10,12 +10,15 @@ import { WRAPPED_SOL_MINT } from "../../utils/ids";
 import { formatUSD } from "../../utils/utils";
 import { useConnection } from "../../contexts/connection";
 import { PublicKey, RpcResponseAndContext, TokenAmount } from "@solana/web3.js";
+import Countdown from 'react-countdown';
+import Moment from 'react-moment';
+import moment, { Duration } from 'moment';
 export const HomeView = () => {
   const { marketEmitter, midPriceInUSD } = useMarkets();
   const { tokenMap } = useConnectionConfig();
   const [tokenTotal, setTokenTotal] = useState<TokenAmount>()
   const connection = useConnection()
-
+  const [timeTo, setTimeTo] = useState<Duration>()
   const SHDW_ADDRESS = "C3ciBtHiEFnwi7q2GnGGUoN3dFiCSMsKRY3CY5JziXuy";
   const SOL = useUserBalance(WRAPPED_SOL_MINT);
   // const SHDW_ACC = useAccountByMint('C3ciBtHiEFnwi7q2GnGGUoN3dFiCSMsKRY3CY5JziXuy')
@@ -29,7 +32,9 @@ export const HomeView = () => {
       let tokenInfo = await connection.getTokenAccountBalance(SHDW_KEY);
       setTokenTotal(tokenInfo.value)
     })()
-    console.log(tokenTotal)
+    let idoClose = moment.duration(moment('4 Jan 2022 09:00:00 EST').diff(moment()));
+    console.log(idoClose.hours())
+    setTimeTo(idoClose)
     const dispose = marketEmitter.onMarket(() => {
       refreshTotal();
     });
@@ -51,6 +56,11 @@ export const HomeView = () => {
               Amount needed before Franks Foot Collection : {formatUSD.format(100000000 - parseInt(tokenTotal.uiAmountString as string))}
             </h2>
             <h2>We're {((parseInt(tokenTotal.uiAmountString as string) / 100000000) * 100).toFixed(2)}% of the way to Frank releasing a Foot Pic NFT Collection! </h2>
+            <h2>The IDO ends in: {' '}<Countdown date={Date.parse("4 Jan 2022 09:00:00 EST")} zeroPadTime={0}></Countdown></h2>
+            {timeTo? (
+              <h2>To reach our goal we need to raise {formatUSD.format((100000000 -parseInt(tokenTotal.uiAmountString as string))/timeTo?.hours())} per hour</h2>
+            ):(<h2>Could not calculate remaining raise rate</h2>)}
+            
           </div>
         ) : (
           <h2> Could not retrieve token </h2>)}
